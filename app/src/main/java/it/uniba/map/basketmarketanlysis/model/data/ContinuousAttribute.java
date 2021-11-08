2d8c39a98a4e74cb80da2ed87ac3a5733f82df63
Gianluca Calò <gianluca.calo11@gmail.com>
2021-11-08 13:16:08 +0100

    aggiunti ulteriori commenti

--- app/src/main/java/it/uniba/map/basketmarketanlysis/model/data/ContinuousAttribute.java
+++ app/src/main/java/it/uniba/map/basketmarketanlysis/model/data/ContinuousAttribute.java
@@ -7,22 +7,35 @@ public class ContinuousAttribute extends Attribute implements Iterable<Float> {
     private final float max;
     private final float min;
 
+    /**
+     * Costruttore di Continuous Attribute
+     * @param name string nomee dell'attributo
+     * @param index indice dell'attributo
+     * @param min minimo tra i valori dell'attributo
+     * @param max massimo tra i valori dell'attributo
+     */
     public ContinuousAttribute(String name, int index, float min, float max) {
         super(name, index);
         this.min = min;
         this.max = max;
     }
 
+    /**
+     * @return max massimo tra i valori dell'attributo
+     */
     public float getMax() {
         return max;
     }
 
+    /**
+     * @return min minimo tra i valori dell'attributo
+     */
     public float getMin() {
         return min;
     }
 
     /**
-     * Restituisce l'iteratore
+     * Restituisce un iteratore che discretizza l'intervallo tra minimo e massimo (escluso)
      * @return
      */
     @Override
--- app/src/main/java/it/uniba/map/basketmarketanlysis/model/mining/ComparatorGrowRate.java
+++ app/src/main/java/it/uniba/map/basketmarketanlysis/model/mining/ComparatorGrowRate.java
@@ -2,6 +2,10 @@ package it.uniba.map.basketmarketanlysis.model.mining;
 
 import java.util.Comparator;
 
+/**
+ * Classe funzionale (un solo metodo) che compare due emergin pattern
+ * in base al grow rate
+ */
 public class ComparatorGrowRate implements Comparator<EmergingPattern> {
 
     @Override
